647605189f2bda7acaba0b125991c34b5c1c69fb
Gabriel <gabitogol@gmail.com>
2023-07-13 07:25:41 +0200

    .

--- src/routes/usecases/+page.svelte
+++ src/routes/usecases/+page.svelte
@@ -3,5 +3,5 @@
 </script>
 <a href="{base}/">Home</a>
 <a href="{base}/usecases">Usecases</a>
-<h1>Welcome to SvelteKit</h1>
+<h1>Usecases</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
